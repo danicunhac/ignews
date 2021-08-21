ca12ecfde1c730dc4194e09ab8c759f8dfa16894
Daniel Cunha <danielcc.jp@gmail.com>
2021-08-21 10:25:15 -0300

    refac: add check for existing user inside fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -14,18 +14,34 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
-    async signIn(user, account, profile) {
+    async signIn(user) {
       const { email } = user
       try {
         await fauna.query(
-          q.Create(q.Collection('users'), {
-            data: { email },
-          })
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(q.Collection('users'), {
+              data: { email },
+            }),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
         )
 
         return true
       } catch {
-        return true
+        return false
       }
     },
   },
